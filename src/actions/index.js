ecfbd1e8ca2de1cfa7a260944603b17faac4ab40
Felipe De la Fuente <fadelafuente1@gmail.com>
2018-03-31 02:10:51 -0300

    delete extra node modules

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const SHOW_USERS = 'SHOW_USERS'
+
+
+export function showUsers(){
+    return(dispatch, getState) => {
+        axios.get('http://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                console.log(response)
+                dispatch({type: SHOW_USERS, payload: response.data})
+            }
+        )
+    }
+    
+}
